249b5192006ff0b0665056144ca64781a7edbce7
Tarciso Nascimento <tarcisoedf@gmail.com>
2021-06-24 17:05:34 -0400

    Inicio da criaçao do modulo Treino Tela Macrociclo inicio da ediçao llayout

--- src/br/com/sa/telas/TelaMacrociclo.java
+++ src/br/com/sa/telas/TelaMacrociclo.java
@@ -35,15 +35,73 @@ public class TelaMacrociclo extends javax.swing.JInternalFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel2 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jSeparator1 = new javax.swing.JSeparator();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jSeparator2 = new javax.swing.JSeparator();
+        jSeparator3 = new javax.swing.JSeparator();
+        jSeparator4 = new javax.swing.JSeparator();
+        jLabel1 = new javax.swing.JLabel();
+
+        setClosable(true);
+
+        jPanel2.setBackground(new java.awt.Color(255, 247, 241));
+        jPanel2.setLayout(null);
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/com/sa/icones/macro700x63.png"))); // NOI18N
+        jLabel2.setToolTipText("");
+        jPanel2.add(jLabel2);
+        jLabel2.setBounds(330, 0, 700, 63);
+
+        jLabel3.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel3.setText("Parâmetros do Macrociclo");
+        jPanel2.add(jLabel3);
+        jLabel3.setBounds(10, 322, 310, 30);
+        jPanel2.add(jSeparator1);
+        jSeparator1.setBounds(30, 350, 270, 10);
+
+        jLabel4.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel4.setText("Parâmetros da Avaliação Física");
+        jPanel2.add(jLabel4);
+        jLabel4.setBounds(20, 3, 290, 30);
+
+        jLabel5.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel5.setText("Neuromusculares");
+        jPanel2.add(jLabel5);
+        jLabel5.setBounds(20, 45, 140, 30);
+
+        jLabel6.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jLabel6.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel6.setText("Cardiopulmonares");
+        jPanel2.add(jLabel6);
+        jLabel6.setBounds(20, 177, 140, 30);
+        jPanel2.add(jSeparator2);
+        jSeparator2.setBounds(30, 30, 270, 10);
+        jPanel2.add(jSeparator3);
+        jSeparator3.setBounds(30, 70, 150, 10);
+        jPanel2.add(jSeparator4);
+        jSeparator4.setBounds(30, 202, 150, 10);
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/com/sa/icones/fundoMacro640x335.png"))); // NOI18N
+        jPanel2.add(jLabel1);
+        jLabel1.setBounds(-1, -1, 335, 640);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1033, Short.MAX_VALUE)
+            .addComponent(jPanel2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 1032, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 640, Short.MAX_VALUE)
+            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, 639, Short.MAX_VALUE)
         );
 
         pack();
@@ -51,5 +109,16 @@ public class TelaMacrociclo extends javax.swing.JInternalFrame {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JSeparator jSeparator2;
+    private javax.swing.JSeparator jSeparator3;
+    private javax.swing.JSeparator jSeparator4;
     // End of variables declaration//GEN-END:variables
 }
